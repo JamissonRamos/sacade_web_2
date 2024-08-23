9b4fe026e0480b7f54362eb308298fbd25821d19
Jamisson Lira Ramos <missonlira@gmail.com>
2024-08-23 18:25:21 -0300

    Criando componente que vai ser base para maiori das pages

---
+++ src/components/Wrappe/pages/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"; 
+import { Theme } from "../../../theme";
+
+export const WrapPages = styled.div`
+/* border: 1px solid blue; */
+    width: 100%;
+    height: 100%; 
+    padding: 1.4rem .8rem;
+    border-radius: 8px;
+    overflow-y: auto;
+    box-shadow: ${Theme.Shadow.sh800};  
+    transition: padding 0.4s, margin 0.4s;
+    @media (max-width: 768px) {
+        padding: .8rem;
+    }
+    @media (max-width: 425px) {
+        margin: 0;
+        box-shadow: none;
+    }
+    
+`;
