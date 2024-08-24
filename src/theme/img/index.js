8899f15aa6c044b475a84b589a02ff88213b0c52
Jamisson Lira Ramos <missonlira@gmail.com>
2024-08-24 17:07:37 -0300

    Criando pasta para armazena endereço de img

---
+++ src/theme/img/index.js
@@ -0,0 +1,16 @@
+
+import Logo from '../../assets/logo.png'
+import GolpeQueda from '../../assets/golpeQueda.png'
+import Security from '../../assets/security.png'
+import PublishArticle from '../../assets/publishArticle.png'
+import AvatarMan from '../../assets/avatar_man.png'
+import LogoLuta from '../../assets/logo_luta.png'
+
+export const ImgC = {
+    Logo,
+    GolpeQueda,
+    Security,
+    PublishArticle,
+    AvatarMan,
+    LogoLuta
+}
