e3262113dc1fcf8fdf7583cfa82d4d4e1fd75e25
Jamisson Lira Ramos <missonlira@gmail.com>
2024-08-24 11:50:25 -0300

    Criando Aquivo de todos os icons do projeto

---
+++ src/theme/icons/index.js
@@ -0,0 +1,37 @@
+import * as MD from "react-icons/md";
+import * as RiIcons from 'react-icons/ri';
+import * as Fa6 from "react-icons/fa6";
+import * as IM from "react-icons/im";
+
+export const Icons = {  
+
+    MdHome:                     MD.MdHome,
+    MdOutlineArrowForwardIos:   MD.MdOutlineArrowForwardIos,
+    MdOutlineArrowBackIos:      MD.MdOutlineArrowBackIos,
+    MdAddCircle:                MD.MdAddCircle,
+    MdPerson:                   MD.MdPerson,
+    MdAttachMoney:              MD.MdAttachMoney,
+    MdLogout:                   MD.MdLogout,
+    MdSupervisedUserCircle:     MD.MdSupervisedUserCircle,
+    MdMenu:                     MD.MdMenu,
+    MdSearch:                   MD.MdSearch,
+    MdDelete:                   MD.MdDelete,
+    MdEdit:                     MD.MdEdit,
+    MdAdd:                      MD.MdAdd,
+    MdClose:                    MD.MdClose,
+    MdLock:                     MD.MdLock,
+    MdLockPerson:               MD.MdLockPerson,
+    MdVisibility:               MD.MdVisibility,
+    MdVisibilityOff:            MD.MdVisibilityOff,
+    MdSettings:                 MD.MdSettings,
+    MdLockReset:                MD.MdLockReset,
+    MdAccountCircle:            MD.MdAccountCircle,
+    MdContactPhone:             MD.MdContactPhone,
+    MdArrowForward:             MD.MdArrowForward,
+    MdArrowBack:                MD.MdArrowBack,
+    MdCancel:                   MD.MdCancel,
+    RiArrowDownSFill:           RiIcons.RiArrowDownSFill,
+    RiArrowUpSFill:             RiIcons.RiArrowUpSFill,
+    FaMapLocationDot:           Fa6.FaMapLocationDot,
+    ImSwitch:                   IM.ImSwitch
+}
