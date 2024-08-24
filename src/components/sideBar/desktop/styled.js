92ce4a411ddfd5607f84be8f5c2caf1f4b436440
Jamisson Lira Ramos <missonlira@gmail.com>
2024-08-24 11:52:41 -0300

    Criando o css do sidebar web desktop

---
+++ src/components/sideBar/desktop/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    /* border:1px solid blue; */
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    z-index: 1000;
+
+`;
