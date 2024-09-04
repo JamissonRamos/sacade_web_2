0d9a98bc0d4cf83d028988e75ab60fe9d24e1b6b
Jamisson Lira Ramos <missonlira@gmail.com>
2024-09-04 18:41:47 -0300

    Update index fields

--- src/pages/register/fields/index.js
+++ src/pages/register/fields/index.js
@@ -1,11 +1,9 @@
 import DataUser from './fields_data_user';
-import Contact from './fields_contact';
 import Address from './fields_address';
 
 
 export const FieldUsers = {
     DataUser,
-    Contact,
     Address
 }
 
