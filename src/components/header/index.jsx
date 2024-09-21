8c727884831ca07932b418ccd72e7fc6a4f2740f
Jamisson Lira Ramos <missonlira@gmail.com>
2024-09-21 18:31:01 -0300

    Update name user Logged

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import * as S from './style'
 import { TextC } from '../Typography'
+import { useAuth } from "../../contexts/authContext/AuthContex"
 
 const Header = () => {
+  const { currentUser } = useAuth()
+  const { firstName } = currentUser || {}
+  console.log(currentUser);
+  
   return (
     <S.Header>
       <S.UserLogged>
         <TextC.Title level={1}>Olá,</TextC.Title>
-        <TextC.Title level={1}>Usuário Logado</TextC.Title> 
+        <TextC.Title level={1}>{firstName}</TextC.Title> 
       </S.UserLogged> 
     </S.Header>
   )
