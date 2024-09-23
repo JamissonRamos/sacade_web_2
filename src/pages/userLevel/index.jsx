f7f01f988491c56d11e254119b80dbc336da85f2
Jamisson Lira Ramos <missonlira@gmail.com>
2024-09-23 10:23:44 -0300

    Create page user level

---
+++ src/pages/userLevel/index.jsx
@@ -0,0 +1,11 @@
+import { WrapPages } from '../../components/Wrappe/pages'
+
+const UserLevel = () => {
+  return (
+    <WrapPages>
+      UserLevel
+    </WrapPages>
+  )
+}
+
+export default UserLevel
