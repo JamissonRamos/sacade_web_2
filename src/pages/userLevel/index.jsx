630afa42e3225f558e015fc36bb2e1993916e63e
Jamisson Lira Ramos <missonlira@gmail.com>
2024-09-23 11:17:16 -0300

    Update index user level

--- src/pages/userLevel/index.jsx
+++ src/pages/userLevel/index.jsx
@@ -1,11 +1,26 @@
 import { WrapPages } from '../../components/Wrappe/pages'
+import * as S from './styled';
+import { TextC } from '../../components/Typography'
 
 const UserLevel = () => {
   return (
-    <WrapPages>
-      UserLevel
-    </WrapPages>
-  )
+    <S.Content>
+      <S.TableHeader>
+        <S.TableRow>
+          <S.TableHeaderCell>
+            <TextC.Label level={1}>#</TextC.Label>
+          </S.TableHeaderCell>
+          <S.TableHeaderCell $flex={2}>
+            <TextC.Label level={1}>Nome</TextC.Label>
+          </S.TableHeaderCell>
+          <S.TableHeaderCell>
+            <TextC.Label level={1}>Status</TextC.Label>
+          </S.TableHeaderCell>
+
+        </S.TableRow>
+      </S.TableHeader>
+    </S.Content>
+  );
 }
 
 export default UserLevel
