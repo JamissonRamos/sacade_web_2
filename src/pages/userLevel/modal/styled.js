10bcd84bd97f052640aa1c92e8f23884c0c68eae
Jamisson Lira Ramos <missonlira@gmail.com>
2024-09-26 11:15:15 -0300

    Creat modal userLevel

---
+++ src/pages/userLevel/modal/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { Theme } from "../../../theme";
+
+export const Name = styled.div`
+    /* border: 1px solid red; */
+    /* margin-top: .2rem; */
+    & span {
+        color: ${Theme.Colors.grey600}
+    }
+
+`;
+export const WrapFormLabel = styled.div`
+    /* border: 1px solid red; */
+    display: flex;
+    align-items: center;
+    gap: .8rem;
+`;
