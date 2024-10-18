1ad676bf39313a5513089f84d2240ae09010604b
Jamisson Lira Ramos <missonlira@gmail.com>
2024-10-18 17:18:02 -0300

    Update menu mobile end config form students

--- src/pages/students/components/forms_components/body/styled.js
+++ src/pages/students/components/forms_components/body/styled.js
@@ -65,7 +65,7 @@ const StyledButton = css`
 export const Container = styled.div`
     /* border: 1px solid red; */
     width: 100%;
-    height: 100%;
+    height: 80%;
     display: flex;
     flex-direction: column;
     overflow: auto;
