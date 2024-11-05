3efa87efa33c94445c894ab03807012ea96ce64e
Jamisson Lira Ramos <missonlira@gmail.com>
2024-11-05 14:15:00 -0300

    Atualização styled card

--- src/pages/responsible_students/responsibleList/listResponsible/styled.js
+++ src/pages/responsible_students/responsibleList/listResponsible/styled.js
@@ -58,11 +58,10 @@ const StyledText = css`
 
 export const Content = styled.div`
     /* border: 1px solid red; */
-    /* height: 100%; */
     display: flex;
     flex-direction: column;
     gap: 16px;
-    padding: 0 .8rem; //Quando gerar barra de rolagem;
+    padding: .8rem; //Quando gerar barra de rolagem;
     overflow: auto;
 `;
 export const CardItem = styled.div`
@@ -73,8 +72,8 @@ export const CardItem = styled.div`
     gap: 4px;
     padding: 1rem; // definir um espaço de elemento;
     border: .75px solid ${Theme.Colors.grey400};
+    box-shadow: ${Theme.Shadow.sh900};
     border-radius: 12px;
-    /* background-color: ${Theme.Colors.grey200}; */
     @media (max-width: 768px) {
         display: grid;
         grid-template-columns: repeat(1, 1fr);
@@ -159,112 +158,3 @@ export const WrapButtonEdit = styled.button`
     ${StyledEdit}
     border-color: ${Theme.Colors.green800};
 `;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const TableHeader = styled.div`
-//     /* border: 1px solid blue; */
-//     width: 100%;
-//     height: 30px;
-//     background-color: transparent;
-// `;
-// export const TableRow = styled.div`
-//     border: 1px solid red;
-//     width: 100%;
-//     min-width: 98%;
-//     display: flex;
-//     border-bottom: 1px solid ${Theme.Colors.grey400};
-//     &:nth-child(even) {
-//         background-color: transparent;
-//     }
-//     @media (max-width: 768px) {
-//         & .flex-Colum {
-//             flex-direction: column;
-//         }
-//     }
-// `;
-// export const TableHeaderCell = styled.div`
-//     border: 1px solid red;
-//     display: flex;
-//     align-items: center;
-//     flex: ${({$flex}) => $flex ? $flex : 1};    
-//     /* padding: 8px; */
-//     padding: .6rem 10px;
-//     /* border-bottom: 1px solid ${Theme.Colors.grey300}; */
-//     & > span {
-//         font-style: normal;
-//         font-weight: 800;
-//         font-size: 12.8px;
-//         line-height: 13px;
-//         text-align: left;
-//         text-transform: uppercase;
-//         color: ${Theme.Colors.grey500};
-//     }
-// `;
-// export const TableBody = styled.div`
-//     /* border: 1px solid blue; */
-//     width: 100%;
-//     height: calc(100% - 30px);
-//     padding-bottom: 1rem;
-//     overflow-y: auto;
-// `;
-// export const TableBodyCell = styled.div`
-//     /* border: 1px solid red; */
-//     display: flex;
-//     align-items: center;
-//     justify-content: start;
-//     gap: 1rem;
-//     flex: ${({$flex}) => $flex ? $flex : 1};    
-//     padding: .6rem 10px;
-//     &.status {
-//         text-transform: uppercase;
-//     }
-
-// `;
-// export const CircleLetterName = styled.div`
-//     width: 40px;
-//     height: 40px;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     background: ${Theme.Colors.blue100};
-//     border-radius: 20px;
-//     font-weight: 900;
-//     font-size: 21.1px;
-//     line-height: 25px;
-//     text-transform: uppercase;
-//     color: ${Theme.Colors.blue800};
-//     @media (max-width: 768px) {
-//         width: 30px;
-//         height: 30px;
-//         font-size: 16.1px;
-//     }
-// `;
-// export const WrapListButtons = styled.div`
-//     /* border: 1px solid red; */
-//     width: 100%;
-//     height: 28px;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     gap: 12px;
-
-//     & button {
-//         ${StyledButton}
-//     } 
-
-    
-// `;
