5eb4eba6cabf16016f8c04d8c121903c1344dfe4
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-01 19:26:53 -0300

    Ajuste de form configuration_installments useEffect

--- src/pages/configuration_installments/index.jsx
+++ src/pages/configuration_installments/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as S from './styled';
 import { useConfigurationInstallments } from '../../hooks/configuration_installments';
 import { useEffect, useState } from 'react';
