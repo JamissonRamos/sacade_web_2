021d2167bc5b1a0b25fc69518354b9fe1b11a807
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-02 16:38:17 -0300

    update page  Generate Installments header

---
+++ src/pages/generate_installments/header/index.jsx
@@ -0,0 +1,13 @@
+import * as S from "./styled"
+import { TextC } from "../../../components/Typography"
+
+const Header = () => {
+    return (
+        <S.Container>
+            <TextC.Title level={2}> Gerar Parcelas</TextC.Title>
+            <TextC.Body level={1}> Selecione os alunos para gerar as parcelas </TextC.Body>
+        </S.Container>
+    ) 
+}
+
+export default Header
--- src/pages/generate_installments/index.jsx
+++ src/pages/generate_installments/index.jsx
@@ -1,7 +1,34 @@
+import * as S from './styled';
+
+import { WrapPages } from "../../components/Wrappe/pages";
+import Header from './header';
+import { LoadingOverlay } from '../../components/spinner/global/styled';
+import { Spinner } from 'react-bootstrap';
 
 const GenerateInstallments = () => {
+    const loading = false;
     return (
-        <div>Generate Installments</div>
+        <WrapPages>
+            <S.Content>
+                <Header />
+                {
+                    loading ?
+                    <>
+                        <LoadingOverlay>
+                                <Spinner
+                                    variant='warning'
+                                    as="span"
+                                    animation="border"
+                                    role="status"
+                                    aria-hidden="true"
+                                />
+                                <span className="sr-only">Carregando os dados...</span>
+                            </LoadingOverlay> 
+                    </> : null
+                    // <Form registered={registered}/>
+                }
+            </S.Content>
+        </WrapPages>
     )
 }
 
