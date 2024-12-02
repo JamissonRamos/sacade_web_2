bf35597fdd7a80be92961cc01db4205103610eed
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-02 17:03:46 -0300

    update page  Generate Installments Empty

--- src/pages/generate_installments/index.jsx
+++ src/pages/generate_installments/index.jsx
@@ -4,29 +4,45 @@ import { WrapPages } from "../../components/Wrappe/pages";
 import Header from './header';
 import { LoadingOverlay } from '../../components/spinner/global/styled';
 import { Spinner } from 'react-bootstrap';
+import { TextC } from '../../components/Typography';
 
 const GenerateInstallments = () => {
     const loading = false;
+
     return (
         <WrapPages>
             <S.Content>
                 <Header />
                 {
-                    loading ?
-                    <>
+                    loading 
+                    ?   <>
                         <LoadingOverlay>
-                                <Spinner
-                                    variant='warning'
-                                    as="span"
-                                    animation="border"
-                                    role="status"
-                                    aria-hidden="true"
-                                />
-                                <span className="sr-only">Carregando os dados...</span>
+                            <Spinner
+                                variant='warning'
+                                as="span"
+                                animation="border"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            <span className="sr-only">Carregando os dados...</span>
                             </LoadingOverlay> 
-                    </> : null
-                    // <Form registered={registered}/>
+                        </> 
+                    :   null 
                 }
+
+                
+                {/* Colocar msg sem cadastro ou a lista */}
+
+                <S.Empty>
+                    <TextC.Display level={2} >
+                    Nenhum cadastro
+                    </TextC.Display>
+                    <TextC.Body level={2}>
+                        Não encontramos nenhum cadastro em nossa base de dados.
+                    </TextC.Body>
+                </S.Empty>
+
+
             </S.Content>
         </WrapPages>
     )
--- src/pages/generate_installments/styled.js
+++ src/pages/generate_installments/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Theme } from "../../theme";
 
 export const Content = styled.div`
     /* border: 1px solid red; */
@@ -6,6 +7,27 @@ export const Content = styled.div`
     min-width: 320px;
     height: 100%;
     overflow: auto;
+`;
+export const Empty = styled.div`
+    /* border: 1px solid blue; */
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1rem;
+    text-align: center;
+    color: ${Theme.Colors.green800};
+    @media (max-width: 425px) {
+        padding: 0;
+        & span:first-child{
+            font-size: 1.2rem;
+            font-weight: 500;
+        }
+        & span:last-child{
+            font-size: 1rem;
+        }
+    }
 `;
 
 
