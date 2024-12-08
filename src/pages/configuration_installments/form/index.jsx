c502a82e9d5b29ed8bcca60df525ecfafc3305a4
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-07 22:47:00 -0300

    Update form configuration_installments create method installment

--- src/pages/configuration_installments/form/index.jsx
+++ src/pages/configuration_installments/form/index.jsx
@@ -6,16 +6,19 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { Validations } from '../../validations/index'
 import { Theme } from "../../../theme";
 
-import { FormatPercentageNumber, FormatMoneyNumber, FormattedDate, GenerateInstallments, AddDaysToDate} from "../scripts";
+import { FormatPercentageNumber, FormatMoneyNumber, FormattedDate, AddDaysToDate} from "../scripts";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Fields from "./fields"
 import { Button, Spinner } from "react-bootstrap";
 import { useInstallments } from "../../../hooks/installments";
+import { LoadingOverlay } from "../../../components/spinner/global/styled"
 
 const Form = () => {
     const [fieldDisabled, setFieldDisabled] = useState(true);
+    // const [errorProcess, setErrorProcess] = useState(false);
+    const [loadingProcess, setLoadingProcess] = useState(false);
 
     const navigate = useNavigate();
 
@@ -26,44 +29,65 @@ const Form = () => {
     }); 
 
 
+    const fetchPostCreate = async (installment) => {
+        /* 
+            - Função para gerar parcelas;
+            - O installment é obj para gerar as parcelas;
+        */
+        try {
+            
+            let result = await createDocuments(installment);
+    
+            const {success, message } = result;
+    
+            if(success){
+                console.log(`Parcela ${installment.installmentNumber} foi gerada`);
+                
+            }else{
+                console.error('Error ao criar parcela: ', message);
+                console.error('uid: ', installment.uid);
+            }
+        } catch (error) {
+            console.error('Erro no fetchPostCreate: ', error.message);
+        }
+
+    };
+
     const handleOnSubmit = async (data) => {
-        let result = null;
-        
+        setLoadingProcess(true);
 
-        data.fees = FormatPercentageNumber(data.fees);
-        data.interestAnnual = FormatPercentageNumber(data.interestAnnual);
-        data.interestDaily = FormatPercentageNumber(data.interestDaily);
-        data.interestMonthly = FormatPercentageNumber(data.interestMonthly);
-        data.valueInstallment = FormatMoneyNumber(data.valueInstallment);
-        data.firstDateInstallments = FormattedDate(data.firstDateInstallments);
-
-        console.log('Data: ', data);
-        
+        try {
+            console.log('iniciou');
+            
+            data.fees = FormatPercentageNumber(data.fees);
+            data.interestAnnual = FormatPercentageNumber(data.interestAnnual);
+            data.interestDaily = FormatPercentageNumber(data.interestDaily);
+            data.interestMonthly = FormatPercentageNumber(data.interestMonthly);
+            data.valueInstallment = FormatMoneyNumber(data.valueInstallment);
+            data.firstDateInstallments = FormattedDate(data.firstDateInstallments);
+            
             // Recuperar o documento do local storage
             const studentsUid = JSON.parse(localStorage.getItem('uisStudents')) || [];
             // Verificar se o documento foi encontrado
             if (!studentsUid) {
                 console.log('Erro ao tenta busca Uid de estudantes');
-                //navigate('/notifications/error');
+                navigate('/notifications/error');
             }
+            
+            // Lista para armazenar todas as Promises
+            const allPromises = [];
 
             studentsUid && studentsUid.forEach((uid) => {
-                console.log('uid: ', uid);
-
-                /* 
-                    - 2 for para gerar a parcela e criar o docuento no bnaco;
-                    - passar o uid do estudante;
-                */
-
+    
                 for (let i = 0; i < data.quantityInstallments; i++) {
-                    console.log('parcelas: ', i);
+                    // console.log('parcelas: ', i);
                     let resultDueDate = 0;
-
+    
                     // 1 loop sempre a data da parcela normal 2 loop em diante sempre colocar 30 dias
                     i === 0 
                         ? resultDueDate = data.firstDateInstallments
                         : resultDueDate = AddDaysToDate( data.firstDateInstallments, i)
-
+    
                     const installment = {
                         uid,
                         installmentNumber: `${i + 1}/${data.quantityInstallments}`,
@@ -74,103 +98,91 @@ const Form = () => {
                         interestMonthly: data.interestMonthly,
                         interestDaily: data.interestDaily,
                     };
-
-                        console.log(installment);
-                            
-                    // result = createDocuments();
-
-                    // const {success, message } = result;
-
+    
+                    // installment && fetchPostCreate(installment)
+                      // Adiciona a Promise de criação à lista
+                    if (installment) {
+                        allPromises.push(fetchPostCreate(installment));
+                    }
                 }
-
-                
             })
-
-
-
-       //let resultGenerateInstallments = GenerateInstallments(data)
-
-       // console.log(resultGenerateInstallments);
-        // const {success, installments, message }= resultGenerateInstallments
-        
-        //console.log('installments: ', installments);
-        
-        // if(success){
-        //     console.log('Gerou as PArcelas');
+            // Aguarda todas as Promises serem resolvidas
+            await Promise.all(allPromises);
+            reset()
+            navigate('/notifications/create');
+            console.log('notification create');
             
-        //     //navigate('/notifications/create');
-            
-        // }else{
-        //     console.log('error: ', message);
-        //     //navigate('/notifications/error');
-        // }
-        
-
-
-        
-        // result = {success: true, message: false }
-
-        // const {success, message } = result;
-
-        // if(success){
-        //     console.log('Gerou as PArcelas');
-            
-        //     //navigate('/notifications/create');
-            
-        // }else{
-        //     console.log('error: ', message);
-        //     navigate('/notifications/error');
-        // }
-        //reset()
+        } catch (error) {
+            console.log('error: ', error.message);
+            navigate('/notifications/error');
+        }finally{
+            setLoadingProcess(false)
+            console.log('Finalizou');
+        }
     }
     
     return (
-        <S.Form onSubmit={handleSubmit(handleOnSubmit)} >
-            <Fields 
-                register={register}
-                setValue={setValue}
-                getValues={getValues}
-                errors={errors}
-                fieldDisabled={fieldDisabled}
-                setFieldDisabled={setFieldDisabled}
-            />
+        <>
+            <S.Form onSubmit={handleSubmit(handleOnSubmit)} >
+                <Fields 
+                    register={register}
+                    setValue={setValue}
+                    getValues={getValues}
+                    errors={errors}
+                    fieldDisabled={fieldDisabled}
+                    setFieldDisabled={setFieldDisabled}
+                />
 
-            <S.WrapButtons>
-                <Button
-                    variant="outline-danger"
-                    onClick={() =>  navigate('/')}
-                >
-                    <Theme.Icons.MdClose />
-                    <span>Cancelar</span>
-                </Button>  
-                {
-        
+                <S.WrapButtons>
+                    <Button
+                        variant="outline-danger"
+                        onClick={() =>  navigate('/')}
+                    >
+                        <Theme.Icons.MdClose />
+                        <span>Cancelar</span>
+                    </Button>  
+                    {
                         <Button
-                            variant="success"
-                            type='submit'
-                            disabled={loadingCrate ? true : false}
-                        >   
-                            { loadingCrate ?
-                                <>
-                                    <Spinner
-                                        as="span"
-                                        animation="border"
-                                        size="sm"
-                                        role="status"
-                                        aria-hidden="true"
-                                    />
-                                    <span > Gerando Parcelas... </span>
-                                </> :
-                                <>
-                                    <Theme.Icons.MdAddCard />
-                                    <span>Gerar Parcelas</span>
-                                </>
-                            } 
-                        </Button>  
-                }
+                                variant="success"
+                                type='submit'
+                                disabled={loadingCrate ? true : false}
+                            >   
+                                { loadingCrate ?
+                                    <>
+                                        <Spinner
+                                            as="span"
+                                            animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"
+                                        />
+                                        <span > Gerando Parcelas... </span>
+                                    </> :
+                                    <>
+                                        <Theme.Icons.MdAddCard />
+                                        <span>Gerar Parcelas</span>
+                                    </>
+                                } 
+                            </Button>  
+                    }
 
-            </S.WrapButtons>
-        </S.Form>
+                </S.WrapButtons>
+            {
+                loadingProcess &&
+                    <LoadingOverlay>
+                        <Spinner
+                            as="span"
+                            animation="border"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                        <span >Gerando todas as parcelas...</span>
+                    </LoadingOverlay> 
+            }
+            </S.Form>
+        </>
+
+
     )
 }
 
