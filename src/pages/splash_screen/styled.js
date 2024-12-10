6338859d3db35a5689c5109bcdbd7ea61a5acd9a
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-10 17:16:57 -0300

    Update page SplashScreen paddin container

--- src/pages/splash_screen/styled.js
+++ src/pages/splash_screen/styled.js
@@ -25,9 +25,9 @@ export const Container = styled.div`
     @media (max-width: 768px) {
         padding: .8rem;
     }
-    @media (max-width: 425px) {
+    /* @media (max-width: 425px) {
         padding: 0;
-    }
+    } */
     overflow: auto;
 `;
 
