97146260826bd2b81a2a190e83bf28a6a5c08bb8
Jamisson Lira Ramos <missonlira@gmail.com>
2025-01-15 15:48:20 -0300

    Ajuste de tela de lista de Alunos

--- src/pages/students/styled.js
+++ src/pages/students/styled.js
@@ -4,24 +4,30 @@ import { Theme } from "../../theme";
 export const Empty = styled.div`
     /* border: 1px solid blue; */
     width: 100%;
-    min-width: 320px;
+    min-width: 300px;
     /* height: 80%; */
     flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding: 0 .1rem;
     margin-top: 1rem;
     text-align: center;
     color: ${Theme.Colors.green800};
-    @media (max-width: 425px) {
-        padding: 0;
+    @media (max-width: 768px) {
         & span:first-child{
-            font-size: 1.2rem;
+            font-size: 1.6em;
             font-weight: 500;
         }
         & span:last-child{
-            font-size: 1rem;
+            font-size: 1em;
+        }
+    }
+    @media (max-width: 425px) {
+        & span:first-child{
+            font-size: 1.2rem;
+            font-weight: 500;
         }
     }
 `;
@@ -29,7 +35,7 @@ export const Empty = styled.div`
 export const Content = styled.div`
     /* border: 1px solid red; */
     width: 100%;
-    min-width: 320px;
+    min-width: 310px;
     max-height: 76%;
     padding-bottom: .8rem;
     overflow: auto;
