7d0f5e4163627b4f82959a48421219456cd2ff6d
Jamisson Lira Ramos <missonlira@gmail.com>
2025-03-11 10:59:17 -0300

    Atualização permissoes secoes home

--- src/pages/home/body/index.jsx
+++ src/pages/home/body/index.jsx
@@ -1,18 +1,50 @@
-import RegisterStudent from "../components/register_stuedent";
-import Students from "../components/students";
-import * as S from "./styled";
+import * as S           from "./styled";
+import { useAuth }      from '../../../contexts/authContext/AuthContex'
+import RegisterStudent  from "../components/register_stuedent";
+import Students         from "../components/students";
 
 const Body = () => {
+    const { currentUser } = useAuth(); //Recuperando user logado;
+    const { status } = currentUser  //Recuperando status de user logado;
+
+    //nomeando status 
+    const statusMap = {
+        Administrador: 1,
+        Assistente: 2,
+        Visitante: 3,
+    };
+
+    //atribuindo permicoes as secoes
+    const sectionsAuth = {
+        Students: [ 1, 2, 3],
+        RegisterStudent: [1, 2],
+    };
+
+    //Comparando status com permicoes
+    const handleHasAccess = (sections, status) => {
+        let result
+        const statusIndex = statusMap[status];
+        result = sectionsAuth[sections]?.includes(statusIndex) || false; // Verifica se o status tem permissão    
+        return result
+    };
+
     return (
         <S.Container>
 
-            
             <S.SectionStudents>
-                <Students />
+                {
+                    handleHasAccess('Students', status )
+                    ? <Students />
+                    : null
+                }
             </S.SectionStudents>
 
             <S.SectionRegisterStudents>
-                <RegisterStudent />
+                {
+                    handleHasAccess('RegisterStudent', status )
+                    ? <RegisterStudent />
+                    : null
+                }
             </S.SectionRegisterStudents>
 
             {/* Outra seção de demostrativos */}
