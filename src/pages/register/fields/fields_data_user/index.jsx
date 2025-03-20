ed2044f5941fd1a40ea14e98f44f0dd11b25fa8e
Jamisson Lira Ramos <missonlira@gmail.com>
2025-03-20 18:07:04 -0300

    Atualizando form create limpando o campo com trim

--- src/pages/register/fields/fields_data_user/index.jsx
+++ src/pages/register/fields/fields_data_user/index.jsx
@@ -7,7 +7,6 @@ import { Theme } from '../../../../theme';
 import { TextC } from '../../../../components/Typography';
 import { mask } from 'remask';
 
-
 const DataUser = ({register, setValue, errors}) => {
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
@@ -18,7 +17,7 @@ const DataUser = ({register, setValue, errors}) => {
       const capitalizedWords = inputValue.split(' ').map(word => {
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
       });
-      const newValue = capitalizedWords.join(' ');
+      const newValue = capitalizedWords.join('');
       setValue(e.target.name, newValue); // Atualiza o valor no React Hook Form
   };
 
