82ffd9895facde5c4c67643d54e04a601213cab8
Jamisson Lira Ramos <missonlira@gmail.com>
2025-04-20 10:26:21 -0300

    Adiciona lógica para verificar se o aluno é menor de idade e atualiza a página de pagamentos

--- src/pages/payments/index.jsx
+++ src/pages/payments/index.jsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react';
 import { WrapPages } from '../../components/Wrappe/pages'
+import { useNavigate } from 'react-router-dom';
+import { useStudents } from "../../hooks/students";
+import { AddAttributeList } from './scripts';
 
 const Payments = () => {
+  const [registered, setRegistered] = useState(null);
+  const navigate = useNavigate();
+
+  const { getDocuments, loading} = useStudents.useGetDocuments();
+
+  useEffect(() => {
+      const fetch = async () => {
+        const result = await getDocuments();
+        const { success, data, error} = result;
+        
+        if(success) {
+          // Adiciona o atributo isMinor a cada objeto no array data
+          const newListStudent = await AddAttributeList(data);
+
+          setRegistered(newListStudent)
+          return;
+        }else{
+          console.log('Error ao Busca Student:', error);
+        } 
+      }
+      fetch();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+
+//     const handleNavegation = (uid) =>{
+//         //Recuperar nome compleo to aluno
+//         const foundStudent = registered.find((item) => item.uid === uid);
+//         const {firstName, lastName } = foundStudent;
+
+//         navigate('/plotHistory', { state: { uid: uid, fullName: `${firstName} ${lastName} `} });
+//     }
+  
   return (
     <WrapPages>
       Payments
