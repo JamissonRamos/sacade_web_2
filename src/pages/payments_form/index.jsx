d1f2a3f24003b7c0cc8ca30b9e9274ee09395e29
Jamisson Lira Ramos <missonlira@gmail.com>
2025-05-02 16:33:20 -0300

    Adiciona componente Card para exibir detalhes de parcelas, incluindo cálculo e formatação de juros e taxas. Integra lógica de armazenamento no localStorage e atualiza o formulário de pagamentos.

--- src/pages/payments_form/index.jsx
+++ src/pages/payments_form/index.jsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react';
 import { WrapPages } from '../../components/Wrappe/pages'
 import Header from './header'
+import Card from './card';
+import { Button } from 'react-bootstrap';
 
 const PaymentsForm = () => {
+    const [parcelData, setParcelData] = useState([]); 
+        
+    //loading data from localStorage
+    useEffect(() => {
+        //Recuperar array do localStorage
+        const dataParcel = JSON.parse(localStorage.getItem('parcelData')) || [];
+            
+        //Verificar se o array está vazio
+        if (dataParcel.length === 0) {
+            console.log('parcelData vazio, Alguma coisa errada aconteceu');
+            return
+        }
 
+        setParcelData(dataParcel);
+        
+    }, [])
+
+    console.log('parcelData', parcelData);
+    
     return (
 
         <WrapPages>
             <Header />
+            <Card data={parcelData} />
+            
+            <Button>pagar</Button>
+
+            <hr />
+            <h3>Pagamentos Feitos</h3>
+
+            <ul>
+                <li>data</li>
+                <li>Forma Pagamento</li>
+                <li>Acrescimos</li>
+                <li>Descontos</li>
+                <li>Valor Pago</li>
+            </ul>
+
 
         </WrapPages>
     )
