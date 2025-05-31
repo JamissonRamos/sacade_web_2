40d58230aaa2b222591b138d9371d0ec9af7d465
Jamisson Lira Ramos <missonlira@gmail.com>
2025-05-31 11:16:22 -0300

    refactor: remove payments form components and related styles
    - Deleted ListPay component and its styles.
    - Removed Body component and its styles.
    - Eliminated Card component and its styles.
    - Removed WrepButtons component and its styles.
    - Deleted CreateForm and associated fields components.
    - Removed Header component and its styles.
    - Deleted UpdateForm component.
    - Cleaned up scripts related to payment calculations and formatting.
    - Removed index files for payments form and its subcomponents.

--- src/pages/payments/list_student/styled.js
+++
@@ -1,8 +0,0 @@
-// import styled from "styled-components";
-
-// export const WrapList = styled.div`
-//     /* border: 1px solid red; */
-//     width: 100%;
-//     min-width: 320px;
-//     padding: .2rem;
-// `;
--- src/pages/payments_form/body/components/listPay/styled.js
+++
@@ -1,8 +0,0 @@
-import styled, { css } from "styled-components";
-import { Theme } from "../../../../../theme";
-
-export const Container = styled.div`
-    /* border: 1px solid red; */
-    width: 100%;
-    padding: .2rem 0;
-`;
