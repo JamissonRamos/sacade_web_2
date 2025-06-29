49771af243e5d8a23092394a61acc4948be728e2
Jamisson Lira Ramos <missonlira@gmail.com>
2025-06-29 11:08:21 -0300

    fix: corrigir a cor da borda do container para azul e ajustar a altura em dispositivos pequenos

--- src/pages/reports/tracks/current_student_tracks/Body/styled.js
+++ src/pages/reports/tracks/current_student_tracks/Body/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components"; 
 
 export const Container = styled.div`
-    /* border: 1px solid red; */
+    border: 1px solid red;
     width: 100%;
     min-width: 320px;
     height: 80%;
-    padding-bottom: 2rem;
+    /* padding-bottom: 2rem; */
     @media (max-width: 425px) {
-        /* height: 80%; */
+        height: 80%;
     }
 `;
